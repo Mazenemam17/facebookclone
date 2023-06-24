191c080f59c9bd099756551f6ee7bce194409663
Mazenemam <134785119+Mazenemam@users.noreply.github.com>
2023-06-24 16:49:38 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+var Email = document.forms['form1'] ['Email']
+var Password = document.forms['form1'] ['Password']
+
+var Email_error = document.getElementById('Email_error')
+var Pass_error = document.getElementById('Pass_error')
+
+Email.addEventListener('textInput', Email_Verify);
+Password.addEventListener('textInput', Email_Verify);
+
+function validated(){
+  if (Email.value.length < 9) {
+    Email.style.border = "1px solid red";
+    Email_error.style.display = "block";
+    Email.focus();
+    return false;
+  }
+  if (Password.value.length < 6) {
+    Password.style.border = "1px solid red";
+    Pass_error.style.display = "block";
+    Password.focus();
+    return false;
+  }
+
+}
+
+function Email_Verify() {
+  if (Email.value.length >= 8){
+    Email.style.border = "1px solid rgb(186, 180, 180)";
+    Email_error.style.display = "none";
+    return true;
+  }
+  if (Password.value.length >= 5){
+    Password.style.border = "1px solid rgb(186, 180, 180)";
+    Pass_error.style.display = "none";
+    return true;
+  }
+}
+
+
+
+
+
+// function openform(){
+//   document.getElementById("div3").style.width="380px";
+// }
+
+// function closeform(){
+//     document.getElementById("div3").style.width="0px";
+//   }
